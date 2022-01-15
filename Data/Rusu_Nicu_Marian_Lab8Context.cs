7903da1c51635faea081d629b674d83c23e0ee49
Nicu Marian Rusu <marianrusu@gmail.com>
2022-01-15 17:54:33 +0200

    Add project files.

---
+++ Data/Rusu_Nicu_Marian_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rusu_Nicu_Marian_Lab8.Models;
+
+namespace Rusu_Nicu_Marian_Lab8.Data
+{
+    public class Rusu_Nicu_Marian_Lab8Context : DbContext
+    {
+        public Rusu_Nicu_Marian_Lab8Context (DbContextOptions<Rusu_Nicu_Marian_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Rusu_Nicu_Marian_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Rusu_Nicu_Marian_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Rusu_Nicu_Marian_Lab8.Models.Category> Category { get; set; }
+    }
+}
